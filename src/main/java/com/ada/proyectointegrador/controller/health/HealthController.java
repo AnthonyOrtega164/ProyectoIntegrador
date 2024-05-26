3306c1854111a5e8d4c8c5a52e7f4cc2bbfd81c0
Anthony Ortega <anthony.ortega@enapsys.com>
2024-05-26 11:05:42 -0600

    Cambio de puerto de 8080 -> 8090

--- src/main/java/com/ada/proyectointegrador/controller/health/HealthController.java
+++ src/main/java/com/ada/proyectointegrador/controller/health/HealthController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
 public class HealthController {
 
+    @RequestMapping("/health")
     public String checkAPI() {
         return "<h1>The API is working ok!</h1>";
     }
